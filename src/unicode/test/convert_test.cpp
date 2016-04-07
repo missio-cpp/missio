75b850f39ade0017d76f69f51f7f0d04090a4b08
Ilya Golovenko <ilya@golovenko.com>
2016-04-07 10:27:28 +0300

    [missio.unicode] fixed unit tests

--- src/unicode/test/convert_test.cpp
+++ src/unicode/test/convert_test.cpp
@@ -159,25 +159,34 @@ BOOST_FIXTURE_TEST_CASE(utf32_to_wide_string_test, convert_fixture)
 static char const* original_utf8[] { u8"eﬃcient",   u8"ﬂour",  u8"poſt", u8"poﬅ",  u8"ﬅop",  u8"tschüß",  u8"weiß",  u8"WEIẞ",  u8"ʀᴀʀᴇ" };
 static char const* casefold_utf8[] { u8"efficient", u8"flour", u8"post", u8"post", u8"stop", u8"tschüss", u8"weiss", u8"weiss", u8"ʀᴀʀᴇ" };
 
-BOOST_DATA_TEST_CASE(utf8_fold_string_case_test, boost::unit_test::data::make(original_utf8) ^ casefold_utf8, original, casefold)
+BOOST_AUTO_TEST_CASE(utf8_fold_string_case_test)
 {
-    BOOST_CHECK(missio::unicode::fold_case(std::string(original)) == std::string(casefold));
+    for(std::size_t i = 0; i < std::size(original_utf8); ++i)
+    {
+        BOOST_CHECK(missio::unicode::fold_case(std::string(original_utf8[i])) == std::string(casefold_utf8[i]));
+    }
 }
 
 static char16_t const* original_utf16[] { u"eﬃcient",   u"ﬂour",  u"poſt", u"poﬅ",  u"ﬅop",  u"tschüß",  u"weiß",  u"WEIẞ",  u"ʀᴀʀᴇ" };
 static char16_t const* casefold_utf16[] { u"efficient", u"flour", u"post", u"post", u"stop", u"tschüss", u"weiss", u"weiss", u"ʀᴀʀᴇ" };
 
-BOOST_DATA_TEST_CASE(utf16_fold_string_case_test, boost::unit_test::data::make(original_utf16) ^ casefold_utf16, original, casefold)
+BOOST_AUTO_TEST_CASE(utf16_fold_string_case_test)
 {
-    BOOST_CHECK(missio::unicode::fold_case(std::u16string(original)) == std::u16string(casefold));
+    for(std::size_t i = 0; i < std::size(original_utf16); ++i)
+    {
+        BOOST_CHECK(missio::unicode::fold_case(std::u16string(original_utf16[i])) == std::u16string(casefold_utf16[i]));
+    }
 }
 
 static char32_t const* original_utf32[] { U"eﬃcient",   U"ﬂour",  U"poſt", U"poﬅ",  U"ﬅop",  U"tschüß",  U"weiß",  U"WEIẞ",  U"ʀᴀʀᴇ" };
 static char32_t const* casefold_utf32[] { U"efficient", U"flour", U"post", U"post", U"stop", U"tschüss", U"weiss", U"weiss", U"ʀᴀʀᴇ" };
 
-BOOST_DATA_TEST_CASE(utf32_fold_string_case_test, boost::unit_test::data::make(original_utf32) ^ casefold_utf32, original, casefold)
+BOOST_AUTO_TEST_CASE(utf32_fold_string_case_test)
 {
-    BOOST_CHECK(missio::unicode::fold_case(std::u32string(original)) == std::u32string(casefold));
+    for(std::size_t i = 0; i < std::size(original_utf32); ++i)
+    {
+        BOOST_CHECK(missio::unicode::fold_case(std::u32string(original_utf32[i])) == std::u32string(casefold_utf32[i]));
+    }
 }
 
 BOOST_AUTO_TEST_SUITE_END()
